1011723fc6bba65c6426ffca2944f91f0d01b349
FelipeBriosso <112712605+FelipeBriosso@users.noreply.github.com>
2023-09-03 15:39:59 -0300

    [green] name assign correctly

---
+++ BackEnd/UnitTest/UserTest.cs
@@ -0,0 +1,54 @@
+﻿using BackEnd;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest
+{
+    /// <summary>
+    /// Summary description for UserTest
+    /// </summary>
+    [TestClass]
+    public class UserTest
+    {
+        private User userSample;
+        private const string userSampleName = "userSample";
+        private const string userSampleEmail = "user@Sample.com";
+        private const string userSamplePassword = "userPassword";
+        [TestInitialize]
+        public void Initialize()
+        {
+            userSample = new User();
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void GivenValidNameAssignsToClient()
+        {
+            userSample.Name = userSampleName;
+            Assert.AreEqual(userSampleName, userSample.Name);
+        }
+    }
+}
