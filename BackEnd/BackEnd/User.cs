ab7a55f328a863cc5704392611a62a6c053e4e6f
FelipeBriosso <112712605+FelipeBriosso@users.noreply.github.com>
2023-09-03 20:26:15 -0300

    [green] name length valid

--- BackEnd/BackEnd/User.cs
+++ BackEnd/BackEnd/User.cs
@@ -1,8 +1,25 @@
-﻿namespace BackEnd
+﻿using System.Runtime.InteropServices;
+
+namespace BackEnd
 {
     public class User
     {
-        public string Name { get; set; }
+        protected string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if(!HelperValidator.IsLengthBetween(value,2,10)) {
+                    throw new BackEndException("name is too long");
+                }
+                else
+                {
+                      _name = value;
+                }
+              
+            }
+        }
         public string Email { get; set; }
         public string Password { get; set; }
     }
