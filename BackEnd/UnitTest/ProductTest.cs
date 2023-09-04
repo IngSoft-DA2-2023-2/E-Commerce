48c5e2211734f47f6a2c949b958d18654ec69a68
diegoacu1234 <da222675@fi365.ort.edu.uy>
2023-09-04 17:46:56 -0300

    [green] assign name to a product

---
+++ BackEnd/UnitTest/ProductTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BackEnd;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class ProductTest
+    {
+        const string _nameSample = "name sample";
+        [TestMethod]
+        public void GivenAProductReturrnsItsName()
+        {
+            Product p = new Product();
+            p.Name = _nameSample;
+            
+           Assert.AreEqual(_nameSample,p.Name);
+        }
+    }
+}
