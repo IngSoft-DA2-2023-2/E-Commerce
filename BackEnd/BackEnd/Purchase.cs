fbeb0ca9dd9a4d1afd7b71409c614614bd931743
diegoacu1234 <da222675@fi365.ort.edu.uy>
2023-09-05 19:51:35 -0300

    [green] user assignation to a purchase

---
+++ BackEnd/BackEnd/Purchase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd
+{
+    public class Purchase
+    {
+        public User User { get; set; }
+    }
+}
---
+++ BackEnd/UnitTest/PurchaseTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BackEnd;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class PurchaseTest
+    {
+        [TestMethod]
+        public void GivenPurchaseReturnsUser()
+        {
+            User u = new User();
+
+            Purchase p = new Purchase();
+            p.User = u;
+
+            Assert.AreEqual(u, p.User);
+        }
+    }
+}
