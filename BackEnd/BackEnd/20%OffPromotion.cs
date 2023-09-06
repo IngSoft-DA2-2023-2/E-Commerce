4681c7db16bd55de5d1b95e1b7876c1d1c64fb97
diegoacu1234 <da222675@fi365.ort.edu.uy>
2023-09-06 17:27:25 -0300

    [green] returns 20% off promotion is not applicable if the purchase is empty

---
+++ BackEnd/BackEnd/20%OffPromotion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BackEnd
+{
+    public class Promotion
+    {
+        public bool IsApplicable(Purchase p)
+        {
+            return false;
+        }
+    }
+}
---
+++ BackEnd/UnitTest/20%OffPromotionTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BackEnd;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class PromotionTest
+    {
+        [TestMethod]
+        public void GivenEmptyPurchaseReturnsPromotionIsNotApplicable()
+        {
+            Purchase p = new Purchase();
+            Promotion promotion = new Promotion();
+            
+            Assert.IsFalse(promotion.IsApplicable(p));
+        }
+    }
+}
