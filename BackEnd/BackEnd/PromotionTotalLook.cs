440c2f26ffccb4aa40d9b0c20153d662f5012a63
diegoacu1234 <da222675@fi365.ort.edu.uy>
2023-09-06 18:27:09 -0300

    [green] applicability of 1 item cart for total look promotion

---
+++ BackEnd/BackEnd/PromotionTotalLook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd
+{
+    public class PromotionTotalLook
+    {
+        public bool IsApplicable(Purchase purchaseSample)
+        {
+            return false;
+        }
+    }
+}
---
+++ BackEnd/UnitTest/PromotionTotalLookTest.cs
@@ -0,0 +1,27 @@
+﻿using BackEnd;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    public class PromotionTotalLookTest
+    {
+
+        [TestMethod]
+        public void Given1ItemPurchaseReturnsPromotionIsNotApplicable()
+        {
+            Purchase _purchaseSample = new Purchase();
+            List<Product> _cartSample = new List<Product>();
+            _cartSample.Add(new Product());
+            _purchaseSample.Cart = _cartSample;
+
+            PromotionTotalLook promotionTotalLook = new PromotionTotalLook();
+            Assert.IsFalse(promotionTotalLook.IsApplicable(_purchaseSample));
+        }
+
+    }
+}
