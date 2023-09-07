5b45c263dcaeed74a82b1f3192c56f94c66285d9
FACULTADES\alumnoFI <a>
2023-09-07 17:08:08 -0300

    [refactor] used linq to improve clarity

--- BackEnd/BackEnd/Promotion3x1Fidelity.cs
+++ BackEnd/BackEnd/Promotion3x1Fidelity.cs
@@ -6,49 +6,34 @@ namespace BackEnd
     public class Promotion3x1Fidelity : IPromotionable
     {
         private const int _minQuantity = 3;
+
         public bool IsApplicable(Purchase purchase)
         {
-            List<string> uniqueBrands = purchase.Cart.Select(p => p.Brand).Distinct().ToList();
-
-            foreach (string brand in uniqueBrands)
-            {
-                List<Product> productsOfBrand = purchase.Cart.Where(p => p.Brand == brand).ToList();
-
-                if (productsOfBrand.Count >= _minQuantity)
-                {
-                    return true;
-                }
-            }
-            return false;
+            return purchase.Cart.GroupBy(p => p.Brand)
+                                 .Any(g => g.Count() >= _minQuantity);
         }
 
         public int CalculateDiscount(Purchase purchase)
         {
-            if (!IsApplicable(purchase)) 
+            if (!IsApplicable(purchase))
             {
                 throw new BackEndException("Not applicable promotion");
             }
 
             int currentDiscount = 0;
 
-            List<string> uniqueBrands = purchase.Cart.Select(p => p.Brand).Distinct().ToList();
-
-            foreach (string brand in uniqueBrands)
+            foreach (var group in purchase.Cart.GroupBy(p => p.Brand))
             {
-                List<Product> productsOfBrand = purchase.Cart.Where(p => p.Brand == brand).ToList();
-
-                if (productsOfBrand.Count >= _minQuantity)
+                if (group.Count() >= _minQuantity)
                 {
-                    productsOfBrand.Sort((a,b)=> a.Price.CompareTo(b.Price));
-                    if(productsOfBrand[0].Price+ productsOfBrand[1].Price>currentDiscount)
-                    {
-                        currentDiscount = productsOfBrand[0].Price + productsOfBrand[1].Price;
-                    }
+                    var cheapestProducts = group.OrderBy(p => p.Price)
+                                                .Take(2);
+
+                    currentDiscount += cheapestProducts.Sum(p => p.Price);
                 }
             }
+
             return currentDiscount;
         }
-
     }
 }
-
