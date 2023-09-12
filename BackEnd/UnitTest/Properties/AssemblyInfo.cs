d0b518c03548391f2e0ca8bf58545bad837338b0
nuhalde <93264588+nuhalde@users.noreply.github.com>
2023-09-11 22:33:13 -0300

    Upgrade of .net vesion 6.0

--- BackEnd/UnitTest/Properties/AssemblyInfo.cs
+++ BackEnd/UnitTest/Properties/AssemblyInfo.cs
@@ -1,19 +1,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("UnitTest")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnitTest")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("583ab754-92ad-4477-b1be-a82f64522f9f")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
