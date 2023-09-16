56009d3cda80645886f4bc2e51bd16ed96b14f15
nuhalde <93264588+nuhalde@users.noreply.github.com>
2023-09-16 15:22:58 -0300

    [GREEN] creation of test class for ProductControllerTest done

---
+++ Ecommerce/UnitTest/WebApiModelsTest/Controllers/ProductControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using WebApi.Models.In;
+
+namespace UnitTest.WebApiModelsTest.Controller
+{
+
+    [TestClass]
+    public class ProductControllerTest
+    {
+      
+
+
+        [TestInitialize]
+        public void Init()
+        {
+            
+        }
+
+        
+
+
+
+    }
+}
