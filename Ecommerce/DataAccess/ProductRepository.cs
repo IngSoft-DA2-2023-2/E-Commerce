d6c256a78c6c4100137f1649880a01bc17ddffa6
FelipeBriosso <112712605+FelipeBriosso@users.noreply.github.com>
2023-09-19 14:22:33 -0300

    [green] throws exception creating existing product

--- Ecommerce/DataAccess/ProductRepository.cs
+++ Ecommerce/DataAccess/ProductRepository.cs
@@ -15,9 +15,13 @@ namespace DataAccess
 
         public Product CreateProduct(Product product)
         {
-            _eCommerceContext.Products.Add(product);
-            _eCommerceContext.SaveChanges();
-            return product;
+            if (_eCommerceContext.Products.FirstOrDefault(p => p.Name.Equals(product.Name)) is null)
+            {
+                _eCommerceContext.Products.Add(product);
+                _eCommerceContext.SaveChanges();
+                return product;
+            }
+            throw new ArgumentException($"Product {product.Name} already exists.");
         }
     }
 }
--- Ecommerce/DataAccessTest/ProductRepositoryTest.cs
+++ Ecommerce/DataAccessTest/ProductRepositoryTest.cs
@@ -10,7 +10,7 @@ namespace DataAccessTest
     public class ProductRepositoryTest
     {
         [TestMethod]
-        public void CreateGenreOk()
+        public void CreateProductOk()
         {
             Product product = new Product() {Name = "Sample"};
             var productContext = new Mock<ECommerceContext>();
@@ -18,6 +18,25 @@ namespace DataAccessTest
             IProductRepository productRepository = new ProductRepository(productContext.Object);
             var expectedReturn = productRepository.CreateProduct(product);
             Assert.AreEqual(expectedReturn, product);
+        }
+        [TestMethod]
+        public void CreateAlreadyExistingProduct()
+        {
+            Product product = new Product() {Name = "Sample" };
+            var productContext = new Mock<ECommerceContext>();
+            productContext.Setup(ctx => ctx.Products).ReturnsDbSet(new List<Product>() {product });
+            IProductRepository productRepository = new ProductRepository(productContext.Object);
+            Exception catchedException = null;
+            try
+            {
+                productRepository.CreateProduct(product);
+            }
+            catch (Exception ex)
+            {
+                catchedException = ex;
+            };
+            Assert.IsInstanceOfType(catchedException, typeof(ArgumentException));
+            Assert.IsTrue(catchedException.Message.Equals("Product Sample already exists."));
         }
     }
 }
