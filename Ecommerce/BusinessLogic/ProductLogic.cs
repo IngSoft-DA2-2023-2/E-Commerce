6cb382bb2e1989e8ba02e38cdc54a8303f017580
FelipeBriosso <112712605+FelipeBriosso@users.noreply.github.com>
2023-09-20 17:01:49 -0300

    created Logic class

---
+++ Ecommerce/BusinessLogic/ProductLogic.cs
@@ -0,0 +1,23 @@
+﻿using Domain;
+using LogicInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    internal class ProductLogic : IProductLogic
+    {
+        public Guid AddProduct(Product newProduct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetProducts(string? name, string? brandName, string? categoryName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
