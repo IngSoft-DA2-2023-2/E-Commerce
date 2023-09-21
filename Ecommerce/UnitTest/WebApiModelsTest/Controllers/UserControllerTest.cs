89f2c3f6200f32eb44f320a68a957941b7858286
diegoacu1234 <da222675@fi365.ort.edu.uy>
2023-09-21 18:34:23 -0300

    [green] get all users

---
+++ Ecommerce/UnitTest/WebApiModelsTest/Controllers/UserControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using Domain;
+using LogicInterface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Controllers;
+
+namespace UnitTest.WebApiModelsTest.Controllers
+{
+    [TestClass]
+    public class UserControllerTest
+    {
+
+        [TestMethod]
+        public void GetAllUsersOk()
+        {
+            List<User> usersSample = new()
+            {
+                new User {Email= "mail1@sample.com",Name="name1",Password="password1" },
+                new User {Email= "mail2@sample.com",Name="name2",Password="password2" },
+                new User {Email= "mail3@sample.com",Name="name3",Password="password3" },
+            };
+
+            Mock<IUserLogic> mock = new();
+            mock.Setup(u => u.GetUsers()).Returns(usersSample);
+
+            UserController userController = new(mock.Object);
+            var result = userController.GetAllUsers().Result as OkObjectResult;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(usersSample, result.Value);
+        }
+    }
+}
---
+++ Ecommerce/WebApi/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Domain;
+using LogicInterface;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IUserLogic _userLogic;
+        public UserController(IUserLogic logic)
+        {
+            _userLogic = logic;
+        }
+
+        [HttpGet]
+        public ActionResult<List<User>> GetAllUsers()
+        {
+            try
+            {
+                return Ok(_userLogic.GetUsers());
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
+    
