d437184f636e5216ce4193741e7c80c80591e7e3
diegoacu1234 <da222675@fi365.ort.edu.uy>
2023-09-22 18:28:01 -0300

    [refactor] solved errors at modelApi

--- Ecommerce/ApiModels/UserRequest.cs
+++ Ecommerce/ApiModels/UserRequest.cs
@@ -1,11 +1,17 @@
-﻿namespace WebApi.Models.In
+﻿using Domain;
+
+namespace WebApi.Models.In
 {
     public class UserRequest
     {
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public List<string> Roles { get; set; }
         public string Address { get; set; }
+
+        public User ToEntity()
+        {
+            return new User { Name = Name, Email = Email, Password = Password, Address = Address };
+        }
     }
 }
--- Ecommerce/ApiModels/UserResponse.cs
+++ Ecommerce/ApiModels/UserResponse.cs
@@ -1,13 +1,22 @@
-﻿namespace WebApi.Models.Out
+﻿using Domain;
+
+namespace WebApi.Models.Out
 {
     public class UserResponse
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
-        public List<string> Roles { get; set; }
         public string Address { get; set; }
+        public List<string> Roles { get; set; }
 
+        public UserResponse(User user)
+        {
+            Id = user.Id;
+            Name = user.Name;
+            Email = user.Email;
+            Address = user.Address;
+            Roles = user.Roles;
+        }
     }
 }
