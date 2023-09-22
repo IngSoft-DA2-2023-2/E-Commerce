17c463234e591b7664b6dabe8240aa31178f1ee0
diegoacu1234 <da222675@fi365.ort.edu.uy>
2023-09-22 18:20:23 -0300

    [refactor] new project for ApiModels

---
+++ Ecommerce/ApiModels/UserRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApi.Models.In
+{
+    public class UserRequest
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public List<string> Roles { get; set; }
+        public string Address { get; set; }
+    }
+}
