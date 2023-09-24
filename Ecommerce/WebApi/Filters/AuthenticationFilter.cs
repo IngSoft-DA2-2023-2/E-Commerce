b81e9c20aa100454c205069b8c35b69c25e663df
Nicole Uhalde <nicoleuhalde@gmail.com>
2023-09-24 19:34:23 -0300

    Added AuthenticationFilter class

---
+++ Ecommerce/WebApi/Filters/AuthenticationFilter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApi.Filters
+{
+    public class AuthenticationFilter : Attribute, IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+           
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            string header = context.HttpContext.Request.Headers["Authorization"];
+            if (header is null)
+            {
+                context.Result = new ObjectResult("Authorization header is required.")
+                {
+                    StatusCode = 401
+                };
+            }
+        }
+
+
+    }
+}
