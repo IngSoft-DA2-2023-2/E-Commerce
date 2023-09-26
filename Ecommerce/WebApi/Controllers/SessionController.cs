a33fc49ebb3b5f33bdff1734f66ead9a710a7cd1
diegoacu1234 <da222675@fi365.ort.edu.uy>
2023-09-26 18:47:52 -0300

    [green] created CreateSessionRequest

---
+++ Ecommerce/WebApi/Controllers/SessionController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class SessionController : ControllerBase
+    {
+    }
+}
---
+++ Ecommerce/WebApiModelsTest/In/CreateSessionRequestTest.cs
@@ -0,0 +1,26 @@
+﻿using ApiModels.In;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiModelsTest.In
+{
+    [TestClass]
+    public class CreateSessionRequestTest
+    {
+        private string emailSample = "test@email.com";
+        private string passwordSample = "password";
+        [TestMethod]
+        public void AssignsProperties()
+        {
+            CreateSessionRequest req = new CreateSessionRequest();
+            req.Email = emailSample;
+            req.Password = passwordSample;
+
+            Assert.AreEqual(req.Email, emailSample);
+            Assert.AreEqual(req.Password, passwordSample);
+        }
+    }
+}
