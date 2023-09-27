ee5f9ee55e921fe35a94c755e7a48f91a648f992
FelipeBriosso <112712605+FelipeBriosso@users.noreply.github.com>
2023-09-27 11:17:19 -0300

    [green] purchase created

---
+++ Ecommerce/DataAccess/Repository/PurchaseRepository.cs
@@ -0,0 +1,29 @@
+﻿using DataAccess.Context;
+using DataAccessInterface;
+using DataAccessInterface.Exceptions;
+using Domain;
+
+namespace DataAccess.Repository
+{
+    public class PurchaseRepository : IPurchaseRepository
+    {
+        private readonly ECommerceContext _eCommerceContext;
+        public PurchaseRepository(ECommerceContext eCommerce)
+        {
+            _eCommerceContext = eCommerce;
+        }
+        public Purchase CreatePurchase(Purchase purchase)
+        {
+           
+            _eCommerceContext.Purchases.Add(purchase);
+            _eCommerceContext.SaveChanges();
+            return purchase;
+         
+        }
+
+        public IEnumerable<Purchase> GetPurchases(Guid? id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Ecommerce/DataAccessTest/PurchaseRepositoryTest.cs
@@ -0,0 +1,29 @@
+﻿using DataAccess.Context;
+using DataAccess.Repository;
+using DataAccessInterface;
+using Domain;
+using Moq;
+using Moq.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessTest
+{
+    [TestClass]
+    public class PurchaseRepositoryTest
+    {
+        [TestMethod]
+        public void CreatePurchaseOk()
+        {
+            Purchase purchase = new Purchase() { Id = Guid.NewGuid() };
+            var purchaseContext = new Mock<ECommerceContext>();
+            purchaseContext.Setup(ctx => ctx.Purchases).ReturnsDbSet(new List<Purchase>() { });
+            IPurchaseRepository purchaseRepository = new PurchaseRepository(purchaseContext.Object);
+            var expectedReturn = purchaseRepository.CreatePurchase(purchase);
+            Assert.AreEqual(expectedReturn, purchase);
+        }
+    }
+}
