7210801894b1165bca8024db949bd2a56572709a
FelipeBriosso <112712605+FelipeBriosso@users.noreply.github.com>
2023-09-28 16:18:38 -0300

    [green] check for existing brand

---
+++ Ecommerce/BusinessLogic/BrandLogic.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessInterface;
+using Domain.ProductParts;
+
+namespace BusinessLogic
+{
+    public class BrandLogic
+    {
+        private IBrandRepository _context;
+
+        public BrandLogic(IBrandRepository context)
+        {
+            _context = context;
+        }
+
+        public bool CheckBrand(Brand brand)
+        {
+            return _context.CheckForBrand(brand.Name);
+        }
+    }
+}
---
+++ Ecommerce/BusinessLogicTest/BrandLogicTest.cs
@@ -0,0 +1,33 @@
+﻿using BusinessLogic;
+using DataAccessInterface;
+using Domain;
+using Domain.ProductParts;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicTest
+{
+    [TestClass]
+    public class BrandLogicTest
+    {
+        [TestMethod]
+        public void GivenExistingColorReturnsTrue()
+        {
+            Brand expected = new()
+            {
+                Name = "Brand"
+            };
+
+            Mock<IBrandRepository> repository = new Mock<IBrandRepository>(MockBehavior.Strict);
+            repository.Setup(logic => logic.CheckForBrand("Brand")).Returns(true);
+            var brandLogic = new BrandLogic(repository.Object);
+            var result = brandLogic.CheckBrand(expected);
+            repository.VerifyAll();
+            Assert.IsTrue(result);
+        }
+    }
+}
