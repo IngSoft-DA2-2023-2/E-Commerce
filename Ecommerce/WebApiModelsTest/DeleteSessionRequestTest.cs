93e49050c8788937714335bff9a7a86144c7e365
diegoacu1234 <da222675@fi365.ort.edu.uy>
2023-09-27 22:52:54 -0300

    [green] model in

---
+++ Ecommerce/WebApiModelsTest/DeleteSessionRequestTest.cs
@@ -0,0 +1,22 @@
+﻿using ApiModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiModelsTest
+{
+    [TestClass]
+    public class DeleteSessionRequestTest
+    {
+        [TestMethod]
+        public void AssignsGuid()
+        {
+            Guid guid = Guid.NewGuid();
+            DeleteSessionRequest request = new DeleteSessionRequest { Token = guid };
+
+            Assert.AreEqual(guid, request.Token);
+        }
+    }
+}
