7f7a5f049afaef8cdd1e4d73092385a9f8d6c98a
FelipeBriosso <112712605+FelipeBriosso@users.noreply.github.com>
2023-09-28 16:24:14 -0300

    [green] check for non existing brand

--- Ecommerce/BusinessLogicTest/BrandLogicTest.cs
+++ Ecommerce/BusinessLogicTest/BrandLogicTest.cs
@@ -29,5 +29,21 @@ namespace BusinessLogicTest
             repository.VerifyAll();
             Assert.IsTrue(result);
         }
+
+        [TestMethod]
+        public void GivenNonExistingColorReturnsFalse()
+        {
+            Brand expected = new()
+            {
+                Name = "Brand"
+            };
+
+            Mock<IBrandRepository> repository = new Mock<IBrandRepository>(MockBehavior.Strict);
+            repository.Setup(logic => logic.CheckForBrand("Brand")).Returns(false);
+            var brandLogic = new BrandLogic(repository.Object);
+            var result = brandLogic.CheckBrand(expected);
+            repository.VerifyAll();
+            Assert.IsFalse(result);
+        }
     }
 }
