fe6d6c8cd7065fa6c9be873c06da18260f658427
Nicole Uhalde <nicoleuhalde@gmail.com>
2023-10-01 16:09:40 -0300

    [green] added a migration

---
+++ Ecommerce/DataAccess/Migrations/20231001190757_change_color_to_colour_and_variations.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class change_color_to_colour_and_variations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
