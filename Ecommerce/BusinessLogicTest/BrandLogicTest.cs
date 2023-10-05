7f85514cbb85639da58bac43193bdcb8273cbf39
FelipeBriosso <112712605+FelipeBriosso@users.noreply.github.com>
2023-10-05 10:23:48 -0300

    brand throws exception when not found

--- Ecommerce/BusinessLogicTest/BrandLogicTest.cs
+++ Ecommerce/BusinessLogicTest/BrandLogicTest.cs
@@ -1,6 +1,9 @@
 ﻿using BusinessLogic;
 using DataAccessInterface;
+using DataAccessInterface.Exceptions;
+using Domain;
 using Domain.ProductParts;
+using LogicInterface.Exceptions;
 using Moq;
 using System.Diagnostics.CodeAnalysis;
 
@@ -25,5 +28,29 @@ namespace BusinessLogicTest
             repository.VerifyAll();
             Assert.IsTrue(result);
         }
+        [TestMethod]
+        public void GivennonExistingBrandThrowsException()
+        {
+            Brand expected = new()
+            {
+                Name = "Brand"
+            };
+
+            Mock<IBrandRepository> repository = new Mock<IBrandRepository>(MockBehavior.Strict);
+            repository.Setup(logic => logic.CheckForBrand("Brand")).Throws(new DataAccessException("Brand Brand does not exists"));
+            var brandLogic = new BrandLogic(repository.Object);          
+            Exception catchedException = null;
+            try
+            {
+                brandLogic.CheckBrand(expected);
+            }
+            catch (Exception ex)
+            {
+                catchedException = ex;
+            };
+            Assert.IsInstanceOfType(catchedException, typeof(LogicException));
+            Assert.IsTrue(catchedException.Message.Equals("Brand Brand does not exists"));
+        }
+
     }
 }
