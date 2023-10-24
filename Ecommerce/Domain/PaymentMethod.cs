020e7437823ebd6b40645e38ee897320a47eb2b4
Nicole Uhalde <nicoleuhalde@gmail.com>
2023-10-24 20:18:02 -0300

    Added Object PaymentMethod to Domain

---
+++ Ecommerce/Domain/PaymentMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class PaymentMethod
+    {
+        public Guid Id { get; set; }
+        public string Category { get; set; }
+
+        public string? SubCategoryName { get; set; }
+    }
+}
