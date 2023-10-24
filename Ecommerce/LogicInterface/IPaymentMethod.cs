36f5c39397cb8089ad166c547efe9ba773c58a74
Nicole Uhalde <nicoleuhalde@gmail.com>
2023-10-24 20:21:48 -0300

    Added IPaymentMethod

---
+++ Ecommerce/LogicInterface/IPaymentMethod.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicInterface
+{
+    public interface IPaymentMethod
+    {
+        public int CalculateDiscount(List<Product> cart);
+
+    }
+}
