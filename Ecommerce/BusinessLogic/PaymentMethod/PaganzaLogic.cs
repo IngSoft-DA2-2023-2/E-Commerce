d4ef2699dc3a8d7c40515220f7feb973e6947113
nuhalde <93264588+nuhalde@users.noreply.github.com>
2023-10-28 14:07:30 -0300

    [GREEN] init PaganzaLogic

---
+++ Ecommerce/BusinessLogic/PaymentMethod/PaganzaLogic.cs
@@ -0,0 +1,18 @@
+﻿using Domain;
+using LogicInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.PaymentMethod
+{
+    public class PaganzaLogic : IPaymentMethod
+    {
+        public int CalculateDiscount(List<Product> cart)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Ecommerce/BusinessLogicTest/PaymentMethodTest/PaganzaLogicTest.cs
@@ -0,0 +1,33 @@
+﻿using BusinessLogic.PaymentMethod;
+using BusinessLogic.Promotions;
+using Domain;
+using LogicInterface;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicTest.PaymentMethodTest
+{
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class PaganzaLogicTest
+    {
+        private IPaymentMethod _paganzaLogic;
+        private List<Product> _cartSample;
+
+        private readonly Product _fiftyDollarProduct = new() { Price = 50 };
+
+
+        [TestInitialize]
+        public void Init()
+        {
+            _cartSample = new List<Product>();
+            _paganzaLogic = new PaganzaLogic();
+        }
+
+
+    }
+}
