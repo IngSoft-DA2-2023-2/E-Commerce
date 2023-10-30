6c07d2a67ca5b7ec8df0892958e8c46779282fa3
nuhalde <93264588+nuhalde@users.noreply.github.com>
2023-10-30 18:51:24 -0300

    [GREEN] added CreditCardRepository and their test

---
+++ Ecommerce/DataAccess/Repository/CreditCardRepository.cs
@@ -0,0 +1,26 @@
+﻿using DataAccess.Context;
+using DataAccessInterface.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class CreditCardRepository : ICreditCardRepository
+    {
+        private readonly ECommerceContext _context;
+        public CreditCardRepository(ECommerceContext context)
+        {
+            _context = context;
+        }
+
+        public bool CheckForCreditCard(string Flag)
+        {
+            var creditCard = _context.CreditCards.FirstOrDefault(b => b.Flag.Equals(Flag));
+            if (creditCard is null) throw new DataAccessException($"CreditCard {Flag} does not exists");
+            return true;
+        }
+    }
+}
---
+++ Ecommerce/DataAccessInterface/Exceptions/ICreditCardRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessInterface.Exceptions
+{
+    public interface ICreditCardRepository
+    {
+        public bool CheckForCreditCard(string Flag);
+    }
+}
