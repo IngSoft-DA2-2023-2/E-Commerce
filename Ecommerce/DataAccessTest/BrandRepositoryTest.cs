304efeb78a7a4581f50bf1d88107a211a959da94
nuhalde <93264588+nuhalde@users.noreply.github.com>
2023-11-06 15:12:48 -0300

    [GREEN] GivenExistingBrandReturnsBrands and GivenNonExistingBrandReturnsEmptyList test done

--- Ecommerce/DataAccessTest/BrandRepositoryTest.cs
+++ Ecommerce/DataAccessTest/BrandRepositoryTest.cs
@@ -46,5 +46,26 @@ namespace DataAccessTest
 
 
         }
+
+        [TestMethod]
+        public void GivenExistingBrandReturnsBrands()
+        {
+            Brand brand = new Brand() { Name = "brand" };
+            var brandContext = new Mock<ECommerceContext>();
+            brandContext.Setup(ctx => ctx.Brands).ReturnsDbSet(new List<Brand>() { brand });
+            IBrandRepository brandRepository = new BrandRepository(brandContext.Object);
+            var expectedReturn = brandRepository.GetBrands();
+            Assert.IsTrue(expectedReturn.Contains(brand));
+        }
+
+        [TestMethod]
+        public void GivenNonExistingBrandReturnsEmptyList()
+        {
+            var brandContext = new Mock<ECommerceContext>();
+            brandContext.Setup(ctx => ctx.Brands).ReturnsDbSet(new List<Brand>() { });
+            IBrandRepository brandRepository = new BrandRepository(brandContext.Object);
+            var expectedReturn = brandRepository.GetBrands();
+            Assert.IsTrue(expectedReturn.Count() == 0);
+        }
     }
 }
