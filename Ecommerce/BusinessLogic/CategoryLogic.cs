197702b3176d1b3ac1aea10d1f2122822ca31a91
nuhalde <93264588+nuhalde@users.noreply.github.com>
2023-11-06 16:06:31 -0300

    [GREEN] done with category controller

--- Ecommerce/BusinessLogic/CategoryLogic.cs
+++ Ecommerce/BusinessLogic/CategoryLogic.cs
@@ -1,25 +1,39 @@
 ﻿using DataAccessInterface;
 using DataAccessInterface.Exceptions;
 using Domain.ProductParts;
+using LogicInterface;
 using LogicInterface.Exceptions;
 
 namespace BusinessLogic
 {
-    public class CategoryLogic
+    public class CategoryLogic : ICategoryLogic
     {
-        private readonly ICategoryRepository _context;
+        private readonly ICategoryRepository _categoryRepository;
 
         public CategoryLogic(ICategoryRepository context)
         {
-            _context = context;
+            _categoryRepository = context;
         }
 
         public bool CheckForCategory(Category category)
         {
             try
             {
-                _context.CheckForCategory(category.Name);
+                _categoryRepository.CheckForCategory(category.Name);
                 return true;
+            }
+            catch (DataAccessException e)
+            {
+                throw new LogicException(e);
+            }
+        }
+
+
+        public IEnumerable<Category> GetCategory()
+        {
+            try
+            {
+                return _categoryRepository.GetCategories();
             }
             catch (DataAccessException e)
             {
---
+++ Ecommerce/WebApiModelsTest/Controllers/CategoryControllerTest.cs
@@ -0,0 +1,41 @@
+﻿using Domain.ProductParts;
+using Domain;
+using LogicInterface;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Controllers;
+using BusinessLogic;
+
+namespace WebApiModelsTest.Controllers
+{
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class CategoryControllerTest
+    {
+        [TestMethod]
+        public void GetAllCategories()
+        {
+            Category cat = new Category()
+            {
+                Id = Guid.NewGuid(),
+                Name = "category"
+            };
+            List<Category> categories = new List<Category>() { cat };
+            Mock<ICategoryLogic> categoryLogic = new Mock<ICategoryLogic>();
+            categoryLogic.Setup(p => p.GetCategory()).Returns(categories);
+            CategoryController categoryController = new CategoryController(categoryLogic.Object);
+            var result = categoryController.GetAllCategories() as OkObjectResult;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(categories, result.Value);
+
+        }
+
+
+    }
+}
