bb5440d69535d35c8f453b1703e284835c909102
nuhalde <93264588+nuhalde@users.noreply.github.com>
2023-11-06 20:02:09 -0300

    [green] done getAllRoles

---
+++ Ecommerce/DataAccessTest/StringWrapperRepositoryTest.cs
@@ -0,0 +1,39 @@
+﻿using DataAccess.Context;
+using DataAccess.Repository;
+using DataAccessInterface;
+using Domain.ProductParts;
+using Moq;
+using Moq.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessTest
+{
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class StringWrapperRepositoryTest
+    {
+        [TestMethod]
+        public void GetAllRoles()
+        {
+          
+            StringWrapper stringWrapper = new StringWrapper()
+            {
+                Id = Guid.NewGuid(),
+                Info = "Admin"
+            };
+
+            List<StringWrapper> stringWrappers = new List<StringWrapper>() { stringWrapper };
+            var rolesContext = new Mock<ECommerceContext>();
+            rolesContext.Setup(ctx => ctx.StringListWrappers).ReturnsDbSet(stringWrappers);
+            IStringWrapperRepository stringWrapperRepository = new StringWrapperRepository(rolesContext.Object);
+            var expected = stringWrapperRepository.GetRoles();
+            Assert.IsTrue(expected.Contains(stringWrapper));
+
+        }
+    }
+}
