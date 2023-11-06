d37b927ff2982701a83b99cb5e0c71a28715c152
nuhalde <93264588+nuhalde@users.noreply.github.com>
2023-11-06 14:26:33 -0300

    [green] BrandController test done

---
+++ Ecommerce/WebApi/Controllers/BrandController.cs
@@ -0,0 +1,32 @@
+﻿using ApiModels.In;
+using ApiModels.Out;
+using BusinessLogic;
+using Domain;
+using LogicInterface;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Filters;
+
+namespace WebApi.Controllers
+{
+    [Route("api/products/brands")]
+    [ApiController]
+    public class BrandController : ControllerBase
+    {
+        private readonly IBrandLogic brandLogic;
+
+        public BrandController(IBrandLogic brandLogic)
+        {
+            this.brandLogic = brandLogic;
+        }
+
+        [HttpGet]
+        [AnnotatedCustomExceptionFilter]
+        public IActionResult GetAllBrands()
+        {
+            return Ok(brandLogic.GetBrands());
+        }
+    }
+
+
+}
+
